d4f4d88ae9f4e95507a30b66eae9b2533fbfd48a
root <sasha.lozenko@gmail.com>
2020-05-04 20:26:33 +0300

    changed hello

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(bot_id12)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how1111 are you doing?")
+	bot.reply_to(message, "test mode on?")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
