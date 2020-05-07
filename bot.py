9f36e7bfa1a1c1f7d3adfdea426b4001a20a3435
root <sasha.lozenko@gmail.com>
2020-05-07 19:36:29 +0300

    ipdated bot.py

--- bot.py
+++ bot.py
@@ -4,30 +4,26 @@ from creds import bot_id12
 bot = telebot.TeleBot(bot_id12)
 
 @bot.message_handler(commands=['ali', 'help', 'psi', 'sag', 'temp'])
-	def send_welcome(message):
-		bot.reply_to(message, "hello how low")
+def send_welcome(message):
+	bot.reply_to(message, "hello how low")
 
-		if message.text == "/ali":
-    		bot.send_message(message.from_user.id, "going to ali page")
-	
+	if message.text == "/ali":
+		bot.send_message(message.from_user.id, "going to ali page")
 
-		elif message.text == "/help":
-   		bot.send_message(message.from_user.id, "list of commands")
+	elif message.text == "/help":
+		bot.send_message(message.from_user.id, "list of commands")
 
-		
-		elif message.text == "/psi":
-   		bot.send_message(message.from_user.id, "psi to bar calc")
-	
+	elif message.text == "/psi":
+	   	bot.send_message(message.from_user.id, "psi to bar calc")
 
-		elif message.text == "/sag":
-   		bot.send_message(message.from_user.id, "sag per travel calc")
+	elif message.text == "/sag":
+	   	bot.send_message(message.from_user.id, "sag per travel calc")
 
+	elif message.text == "/temp":
+	   	bot.send_message(message.from_user.id, "temp difference")
 
-		elif message.text == "/temp":
-   		bot.send_message(message.from_user.id, "temp difference")
-
-		else:
-  		bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+	else:
+	  	bot.send_message(message.from_user.id, "for list of commands type /help.")
 
 
 
