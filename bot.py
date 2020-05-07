35f1a2aaff83db652f23ecf0579ea3b6e97357ad
unknown <sasha.lozenko@gmail.com>
2020-05-07 19:09:20 +0300

    removed autoanswer

--- bot.py
+++ bot.py
@@ -7,9 +7,5 @@ bot = telebot.TeleBot(bot_id12)
 def send_welcome(message):
 	bot.reply_to(message, "test mode on?")
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
-
 bot.polling()
 
