681f8d12654a6cc892e20b1bfa2e66351cf5159d
unknown <sasha.lozenko@gmail.com>
2020-05-12 19:07:16 +0300

    barpsi debug1

--- psikpi.py
+++ psikpi.py
@@ -1,14 +1,19 @@
-temp = input("Input from what you want to convert (1,2bar, 18,7psi) : ")
-press1 = int(temp[:-3])
+temp = input("Input the  temperature you like to convert? (e.g., 1.2bar, 17.5psi etc.) : ")
+degree = int(temp[:-3])
 i_convention = temp[-1]
 
-if i_convention() == "psi":
-  result = int(round(degree * 0,0689)
-  o_convention = "Bar"
-elif i_convention.upper() == "bar":
-  result = int(round(press1 * 14,5038)
-  o_convention = "Psi"
-else:
-  print("Input proper convention.")
-  quit()
-print("The temperature in", o_convention, "is", result, "degrees.")
+#i_convention.upper() == "i"
+#print (i_convention)
+
+#psi - bar convention
+#if i_convention.upper() == "i":
+#  result = int(round((9 * degree) / 5 + 32))
+#  o_convention = "bar"
+#bar - psi convention
+#elif i_convention.upper() == "bar":
+#  result = int(round((degree - 32) * 5 / 9))
+#  o_convention = "psi"
+#else:
+#  print("Input proper convention.")
+#  quit()
+#print("The temperature in", o_convention, "is", result, "degrees.")
