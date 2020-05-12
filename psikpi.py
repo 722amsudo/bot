aaeeb62517614f713266edf21919c54a57146518
unknown <sasha.lozenko@gmail.com>
2020-05-12 19:24:41 +0300

    working, have to remove round

--- psikpi.py
+++ psikpi.py
@@ -1,19 +1,21 @@
 temp = input("Input the  temperature you like to convert? (e.g., 1.2bar, 17.5psi etc.) : ")
-degree = int(temp[:-3])
+degree = float(temp[:-3])
 i_convention = temp[-1]
 
 #i_convention.upper() == "i"
 #print (i_convention)
 
+#print (degree)
+
 #psi - bar convention
-#if i_convention.upper() == "i":
-#  result = int(round((9 * degree) / 5 + 32))
-#  o_convention = "bar"
+if i_convention.upper() == "I":
+  result = int(round(degree * 0.0689))
+  o_convention = "bar"
 #bar - psi convention
-#elif i_convention.upper() == "bar":
-#  result = int(round((degree - 32) * 5 / 9))
-#  o_convention = "psi"
-#else:
-#  print("Input proper convention.")
-#  quit()
-#print("The temperature in", o_convention, "is", result, "degrees.")
+elif i_convention.upper() == "R":
+  result = int(round(degree * 14))
+  o_convention = "psi"
+else:
+  print("Input proper convention.")
+  quit()
+print("The pressure in", o_convention, "is", result, o_convention)
