c570803d065510e74732592fc86190ec351859e6
unknown <sasha.lozenko@gmail.com>
2020-05-14 21:08:33 +0300

    moved kglbs to file

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ def start_message(message):
 def send_text(message):
 
 	if message.text == "/kglbs":
-		bot.send_message(message.from_user.id, "going to test function")
+		kglbs()
 
 	elif message.text == "/help":
 		bot.send_message(message.from_user.id, "list of commands: /kglbs, /psi, /sag, /temp")
--- kglbs.py
+++ kglbs.py
@@ -1,3 +1,8 @@
+
+def kglbs()
+	bot.send_message(message.from_user.id, "going to test function")
+
+
 def my_test(fname):
     one = int(fname)
     result = (one + 2)
