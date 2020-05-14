b8160572757acc9aaad1ad2de676eccb6aabe221
unknown <sasha.lozenko@gmail.com>
2020-05-14 22:12:07 +0300

    minimized bot.py file

--- bot.py
+++ bot.py
@@ -9,23 +9,11 @@ bot = telebot.TeleBot(bot_id12)
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
-@bot.message_handler(commands=['ali', 'help', 'psi', 'sag', 'temp'])
+@bot.message_handler(commands=['ali'])
 def send_text(message):
 
 	if message.text == "/ali":
 		bot.send_message(message.from_user.id, "going to ali page")
-
-	elif message.text == "/help":
-		bot.send_message(message.from_user.id, "list of commands: /ali, /psi, /sag, /temp")
-
-	elif message.text == "/psi":
-	   	bot.send_message(message.from_user.id, "psi to bar calc", psikpifunct ())
-
-	elif message.text == "/sag":
-	   	bot.send_message(message.from_user.id, "sag per travel calc")
-
-	elif message.text == "/temp":
-	   	bot.send_message(message.from_user.id, "temp difference")
 
 	else:
 	  	bot.send_message(message.from_user.id, "for list of commands type /help.")
