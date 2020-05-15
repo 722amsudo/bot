2a5b43744e031ea6a4cbeb2fb2d23f21f18e4d6a
722amsudo <56814490+722amsudo@users.noreply.github.com>
2020-05-15 20:22:03 +0300

    changed command names to to_psi to_bar

--- bot.py
+++ bot.py
@@ -12,16 +12,16 @@ bar = "zzzzz"
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
-@bot.message_handler(commands=['psibar', 'psi', 'bar'])
+@bot.message_handler(commands=['psibar', 'to_psi', 'to_bar'])
 def send_text(message):
 
 	if message.text == "/psibar":
 		bot.send_message(message.from_user.id, "use /to_psi to convert bar-psi or /to_bar to convert psi-bar")
 		
-	elif message.text == "/psi":
+	elif message.text == "/to_psi":
 		bot.send_message(message.from_user.id, test_p)
 		
-	elif message.text == "/bar":
+	elif message.text == "/to_bar":
 		bot.send_message(message.from_user.id, test_z)	
 
 	else:
