f277148e9db02f61cf9fe76ed96ea016aee8687e
722amsudo <56814490+722amsudo@users.noreply.github.com>
2020-05-15 20:07:20 +0300

    added bot message

--- bot.py
+++ bot.py
@@ -12,11 +12,17 @@ test_z = "zzzzz"
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
-@bot.message_handler(commands=['ali'])
+@bot.message_handler(commands=['ali', 'ppp', 'zzz'])
 def send_text(message):
 
 	if message.text == "/ali":
 		bot.send_message(message.from_user.id, test_f)
+		
+	elif message.text == "/ppp":
+		bot.send_message(message.from_user.id, test_p)
+		
+	elif message.text == "/zzz":
+		bot.send_message(message.from_user.id, test_z)	
 
 	else:
 	  	bot.send_message(message.from_user.id, "for list of commands type /help.")
